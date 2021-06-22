70d647ef76963e47db1da1a565f238739174efe3
Pratham443 <77573159+Pratham443@users.noreply.github.com>
2021-06-22 18:00:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var noseX;
+var noseY;
+
+function preload() {
+    mustacheimage = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mustacheimage, noseX, noseY, 70, 50);
+}
+
+function modelLoaded() {
+    console.log("Posenet initialized");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 30;
+        noseY = results[0].pose.nose.y - 10;
+    }
+}
